05dac613c10f0faa621d8a76bf9becedf624b1c4
Haitao Huang <hht1996@hotmail.com>
2020-03-06 04:50:01 -0800

    Finishing Tweet - Part 1

---
+++ Twitter/TweetCell.swift
@@ -0,0 +1,29 @@
+//
+//  TweetCell.swift
+//  Twitter
+//
+//  Created by Haitao Huang on 3/6/20.
+//  Copyright © 2020 Dan. All rights reserved.
+//
+
+import UIKit
+
+class TweetCell: UITableViewCell {
+
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var userNameLabel: UILabel!
+    @IBOutlet weak var tweetContentLabel: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
